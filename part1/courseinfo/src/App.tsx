57b9fe44583ee7e68b3b52e7583082f087845b0d
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-03-03 11:14:55 +0530

    courseinfo: exercise 1.1

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -1,7 +1,69 @@
 export default function App() {
+  const course = "Half Stack application development"
+
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+
+  const part3 = "State of a component"
+  const exercises3 = 14
+
+  const total = exercises1 + exercises2 + exercises3
+
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total total={total} />
     </main>
   )
+}
+
+function Header({ course }: { course: string }) {
+  return <h1>{course}</h1>
+}
+
+type ContentProps = {
+  part1: string
+  exercises1: number
+  part2: string
+  exercises2: number
+  part3: string
+  exercises3: number
+}
+
+function Content({
+  part1,
+  exercises1,
+  part2,
+  exercises2,
+  part3,
+  exercises3,
+}: ContentProps) {
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </>
+  )
+}
+
+function Total({ total }: { total: number }) {
+  return <p>Number of exercises {total}</p>
 }
