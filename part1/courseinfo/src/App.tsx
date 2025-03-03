d0f36f1ea8acabed58851e16b094c48da02d02cf
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-03-03 13:25:42 +0530

    courseinfo: exercise 1.3

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -1,28 +1,27 @@
 export default function App() {
   const course = "Half Stack application development"
 
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  }
 
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  }
 
-  const part3 = "State of a component"
-  const exercises3 = 14
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  }
 
-  const total = exercises1 + exercises2 + exercises3
+  const total = part1.exercises + part2.exercises + part3.exercises
 
   return (
     <main>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
+      <Content part1={part1} part2={part2} part3={part3} />
       <Total total={total} />
     </main>
   )
@@ -32,28 +31,23 @@ function Header({ course }: { course: string }) {
   return <h1>{course}</h1>
 }
 
-type ContentProps = {
-  part1: string
-  exercises1: number
-  part2: string
-  exercises2: number
-  part3: string
-  exercises3: number
+type Part = {
+  name: string
+  exercises: number
 }
 
-function Content({
-  part1,
-  exercises1,
-  part2,
-  exercises2,
-  part3,
-  exercises3,
-}: ContentProps) {
+type ContentProps = {
+  part1: Part
+  part2: Part
+  part3: Part
+}
+
+function Content({ part1, part2, part3 }: ContentProps) {
   return (
     <>
-      <Part partName={part1} numOfExercises={exercises1} />
-      <Part partName={part2} numOfExercises={exercises2} />
-      <Part partName={part3} numOfExercises={exercises3} />
+      <Part partName={part1.name} numOfExercises={part1.exercises} />
+      <Part partName={part2.name} numOfExercises={part2.exercises} />
+      <Part partName={part3.name} numOfExercises={part3.exercises} />
     </>
   )
 }
