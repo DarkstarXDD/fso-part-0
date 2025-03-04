31abaaa1c2cf93522ba406f6892f3f621ca9c5f8
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-03-04 19:00:25 +0530

    anecdotes: exercise 1.12

--- part1/anecdotes/src/App.tsx
+++ part1/anecdotes/src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+
+const anecdotes = [
+  "If it hurts, do it more often.",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  "The only way to go fast, is to go well.",
+]
+
 export default function App() {
+  const [index, setIndex] = useState(getRandomIndex)
+
+  function getRandomIndex() {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    return randomIndex
+  }
+
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <p>{anecdotes[index]}</p>
+      <button onClick={() => setIndex(getRandomIndex)}>Next Anecdote</button>
     </main>
   )
 }
