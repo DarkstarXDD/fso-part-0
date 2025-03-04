52f5492042dec3d739550b7f92e58545e81cd6c7
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-03-04 10:19:00 +0530

    unicafe: exercise 1.6

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react"
+
 export default function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  function handleGoodClick() {
+    setGood(good + 1)
+  }
+
+  function handleNeutralClick() {
+    setNeutral(neutral + 1)
+  }
+
+  function handleBadClick() {
+    setBad(bad + 1)
+  }
+
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <h1>Give Feedback</h1>
+      <div>
+        <Button onClick={handleGoodClick} text="Good" />
+        <Button onClick={handleNeutralClick} text="Neutral" />
+        <Button onClick={handleBadClick} text="Bad" />
+      </div>
+
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+      </ul>
     </main>
   )
+}
+
+function Button({ onClick, text }: { onClick: () => void; text: string }) {
+  return <button onClick={onClick}>{text}</button>
 }
