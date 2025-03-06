9a09b9f118f63fbfa4e126336860b596f303057b
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-03-06 14:31:47 +0530

    phonebook: exercise 2.6

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,7 +1,42 @@
+import { useState, type FormEvent } from "react"
+
 export default function App() {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [newName, setNewName] = useState("")
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setPersons([
+      ...persons,
+      {
+        name: newName,
+      },
+    ])
+    setNewName("")
+  }
+
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <h1>Phonebook</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+
+        <button>Add</button>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
     </main>
   )
 }
